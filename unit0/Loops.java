38f5c1b8b9e609231730459330e88adce540f804
sophlulu <sophialu26@marlborough.org>
2024-09-09 11:53:10 -0700

    homework

---
+++ unit0/Loops.java
@@ -0,0 +1,24 @@
+
+
+public class Loops
+{
+    public static void main(String [] args)
+    
+    {
+        
+        for (int i=0; i<10; i++)
+        {
+            if(i%2 == 0)
+            {
+                
+                System.out.println("Even");
+            {
+            else     
+            }
+                System.out.println(i);
+            }
+            System.out.println(i);
+            
+        }
+    }
+}
