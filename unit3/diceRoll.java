82d363ffa693a3dd144f53b9c13dab77591f972c
sophlulu <sophialu26@marlborough.org>
2024-12-09 13:34:43 -0800

    hw

--- unit3/diceRoll.java
+++ unit3/diceRoll.java
@@ -4,22 +4,24 @@
 public class diceRoll
 {
     
-    public static boolean diceRoll() {
-  
-} 
-  
-    
-     public static void testdiceRoll()
+    public static String diceRoll() 
     {
+        int roll = (int) (Math.random() *6) + 1;
         
-
+        if (roll == 0) return "1";
+        
+        return Integer.toString(roll);
+        
+        
     }
 
-    
-public static void main()
+    public static void main(String[] args)
     {
-    
+        for(int i = 0; i < 7; i++)
+        {
+            System.out.println(diceRoll());
+        }
+    }
+}
    
-}
-}
    
--- unit4/doubleX.java
+++ unit4/doubleX.java
@@ -1,12 +1,50 @@
 boolean doubleX(String str) {
  int i = str.indexOf("x");
-  if (i == -1) return false; // no "x" at all
-
-  // Is char at i+1 also an "x"?
+  if (i == -1) return false; 
+    
   if (i+1 >= str.length()) return false; // check i+1 in bounds?
   return str.substring(i+1, i+2).equals("x");
   
-  // Another approach -- .startsWith() simplifies the logic
-  // String x = str.substring(i);
-  // return x.startsWith("xx");
+  
 }
+
+
+
+//doubleX.java
+
+
+public class doubleX
+{
+    
+    int i = str.indexOf("x");
+  if (i == -1) return false; 
+    
+  if (i+1 >= str.length()) return false; // check i+1 in bounds?
+  return str.substring(i+1, i+2).equals("x");
+  
+}
+    public static void testsdoubleX(String str, int n, String expected, String result)
+    {
+        result = doubleX(str, n);
+        
+        System.out.println("str: " + str +
+                         " num: " + n +
+                " expected: " + expected +
+                " result: " + result);
+
+        if (result.equals(expected))
+            System.out.println("Yes");
+        else
+            System.out.println("No!");
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        teststringTimes("Hi", 2, "HiHi", "HiHi");
+            teststringTimes("Hi", 3, "HiHiHi", "HiHiHi");
+            teststringTimes("Hi", 1, "Hi", "Hi");
+    }
+}
+   
