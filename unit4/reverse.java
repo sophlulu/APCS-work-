d892fdbc54e38dc222464f6fff2fddde1051a237
sophlulu <sophialu26@marlborough.org>
2024-12-12 13:08:23 -0800

    hw

--- unit4/reverse.java
+++ unit4/reverse.java
@@ -3,29 +3,23 @@
 
 public class reverse 
 {
-    public static int reverse(String str1, String str2) 
+    public static String reverse(String input) 
     {
-        int count = 0;
         
-        for(int i = 0; i <= str1.length() - str2.length(); i++) 
-        {
-            if(str1.substring(i, i + str2.length()).equals(str2)) count++;
-        }
-        
-        return count;
+        return"";
     } 
   
     
-    public static void testreverse(String str1, String str2, int expected, int result) 
+    public static void testreverse(String input, String expected) 
     {
-        result = reverse(str1, str2);
+    
+        String result= reverse(input);
         
-        System.out.println("word: " + str1 +
-                " str: " + str2 +
-                " expected: " + expected +
-                " result: " + result + " ");
+        System.out.println(
+                " input: " + input +
+                " expected: " + expected + " ");
 
-        if (result == expected)
+        if (result.equals(expected))
             System.out.println("Yes");
         else
             System.out.println("No!");
@@ -34,9 +28,9 @@ public class reverse
     
     public static void main(String[] args)
     {
-        testreverse("bad", "dab", "dab");
-        testreverse("Hello, world!", "!dlrow ,olleH", "!dlrow ,olleH");
-        testreverse("tacocat", "tacocat", "tacocat");
+        testreverse("bad", "dab");
+        testreverse("Hello, world!", "!dlrow ,olleH");
+        testreverse("tacocat", "tacocat");
         
         
     }
