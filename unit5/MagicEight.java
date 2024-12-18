6b49ab272ab35912de5c2952f21b935fe90de23c
sophlulu <sophialu26@marlborough.org>
2024-12-18 13:03:01 -0800

    hw

--- unit5/MagicEight.java
+++ unit5/MagicEight.java
@@ -3,18 +3,27 @@
 
 public class MagicEight
 {
-    public MagicEight(String shake)
-    {
-          if (Math.random() < )
-            return ;
-        else
-            return ;
-    }
-    
 
-    public void ask()
+    public String ask(String input)
     {
+        double value = Math.random();
         
+          if (value < .1 )
+            return "Yk what HELL YEAH!";
+        else if (value < .2)
+            return " Girl bffr.";
+        else if (value < .3)
+            return "Yeah don't ask me this again..";
+        else if (value < .4)
+            return "2024 the plot ig?";
+        else if (value < .5)
+            return "So you already know the answer to that";
+        else if (value < .6)
+            return "Um.. I don't think you want me to answer this";
+        else if (value < .7)
+            return "Ima be straight with you.. NO";
+        else;
+            return "Try again later";
     }
 
 
--- unit5/MagicEightTest.java
+++ unit5/MagicEightTest.java
@@ -6,7 +6,9 @@ public class MagicEightTest
 {
     public static void main(String[] args)
     {
+        MagicEight ball = new MagicEight();
         
+        System.out.println(ball.ask("How old are you? "));
         
     }
 }
