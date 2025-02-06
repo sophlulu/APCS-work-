84d3bfe49cd07ab1fd67cd9247d168ef909eaa29
sophlulu <sophialu26@marlborough.org>
2025-02-06 09:44:32 -0800

    hw

--- unit9/ReverseALL.java
+++ unit9/ReverseALL.java
@@ -1,76 +1,93 @@
-// ReverseAll.java
+//ReverseAll.java
+
 
 import java.util.*;
-    
+
 
 public class ReverseAll
 {
-  public static String reverse(String s)
-  {
-    String ans = "";
+ public static String reverse(String s)
+ {
+    String ans = "";
 
-    for(int i = s.length() - 1; i >= 0; i--)
-    {
-      ans += s.charAt(i);
-    }
+for(int i = s.length() - 1; i >= 0; i--)
+{
 
-    return ans;
-  }
+ans += s.charAt(i);
 
-  public static ArrayList<String> reverseAll(ArrayList<String> list)
-  {
-    ArrayList<String> reversedList = new ArrayList<String>();
+}
 
-    for(int i = 0; i < list.size(); i++)
-    {
-      reversedList.add(reverse(list.get(i)));
-    }
+return ans;
 
-    return reversedList;
-  }
+}
 
-  public static void testReverseAll(ArrayList<String> list, ArrayList<String> expected)
-  {
-    ArrayList<String> actual = expected;
+public static ArrayList<String> everseAll(ArrayList<String> list)
+    {
+        
+        ArrayList<String> reversedList = new ArrayList<String>();
+        
+        for(int i = 0; i < list.size(); i++)
+        {
+            
+            reversedList.add(reverse(list.get(i)));
+        }
+        
+        return reversedList;
+        
+    }
+    
+    
+    public static void testReverseAll(ArrayList<String> list, ArrayList<String> expected)
+    {
+        ArrayList<String> actual = expected;
+        
+        System.out.print (" \narray reversed list " + expected
+                         + "\nactual reversed list" + actual);
+        
+        if(expected.equals(actual))
+            System.out.println("yay");
+        else 
+            System.out.println ("ew.");
+        
+    }
+    
+    
+   public static void main(String[] args)
+   {
+       ArrayList<String> list1 = new ArrayList<String>();
+       list1.add("abcd");
+       list1.add("xyz");
+       ArrayList<String> exp1 = new ArrayList<String>();
+       exp1.add("dcba");
+       exp1.add("zyx");
+       testReverseAll(list1, exp1);
+       
+       ArrayList<String> list2 = new ArrayList<String>();
+       list2.add("1234");
+       list2.add("5678");
+       ArrayList<String> exp2 = new ArrayList<String>();
+       exp2.add("4321");
+       exp2.add("8765");
+       testReverseAll(list2, exp2);
+       
+       ArrayList<String> list3 = new ArrayList<String>();
+       list3.add("racecar");
+       list3.add("tacocat");
+       list3.add("izzi");
+       ArrayList<String> exp3 = new ArrayList<String>();
+       exp3.add("racecar");
+       exp3.add("tacocat");
+       exp3.add("izzi");
+       testReverseAll(list3, exp3);
+       
+          
+       
+   }
+    
+    
+    
+    
+    
+    
 
-    System.out.print("\narray list: " + list);
-
-    System.out.println("\nexpected reversed list: " + expected
-                        + "\nactual reversed list: " + actual);
-
-    if (expected.equals(actual))
-      System.out.println("yay !!");
-    else
-      System.out.println("ew ! </3");
-
-  }
-
-  public static void main(String[] args)
-  {
-    ArrayList<String> list1 = new ArrayList<String>();
-    list1.add("abcd");
-    list1.add("xyz");
-    ArrayList<String> exp1 = new ArrayList<String>();
-    exp1.add("dcba");
-    exp1.add("zyx");
-    testReverseAll(list1, exp1);
-
-    ArrayList<String> list2 = new ArrayList<String>();
-    list2.add("1234");
-    list2.add("5678");
-    ArrayList<String> exp2 = new ArrayList<String>();
-    exp2.add("4321");
-    exp2.add("8765");
-    testReverseAll(list2, exp2);
-
-    ArrayList<String> list3 = new ArrayList<String>();
-    list3.add("racecar");
-    list3.add("tacocat");
-    list3.add("izzi");
-    ArrayList<String> exp3 = new ArrayList<String>();
-    exp3.add("racecar");
-    exp3.add("tacocat");
-    exp3.add("izzi");
-    testReverseAll(list3, exp3);
-  }
 }
---
+++ unit9/reversesample.java
@@ -0,0 +1,77 @@
+// ReverseAll.java
+
+
+import java.util.*;
+    
+
+public class ReverseAll
+{
+  public static String reverse(String s)
+  {
+    String ans = "";
+
+    for(int i = s.length() - 1; i >= 0; i--)
+    {
+      ans += s.charAt(i);
+    }
+
+    return ans;
+  }
+
+  public static ArrayList<String> reverseAll(ArrayList<String> list)
+  {
+    ArrayList<String> reversedList = new ArrayList<String>();
+
+    for(int i = 0; i < list.size(); i++)
+    {
+      reversedList.add(reverse(list.get(i)));
+    }
+
+    return reversedList;
+  }
+
+  public static void testReverseAll(ArrayList<String> list, ArrayList<String> expected)
+  {
+    ArrayList<String> actual = expected;
+
+    System.out.print("\narray list: " + list);
+
+    System.out.println("\nexpected reversed list: " + expected
+                        + "\nactual reversed list: " + actual);
+
+    if (expected.equals(actual))
+      System.out.println("yay !!");
+    else
+      System.out.println("ew ! </3");
+
+  }
+
+  public static void main(String[] args)
+  {
+    ArrayList<String> list1 = new ArrayList<String>();
+    list1.add("abcd");
+    list1.add("xyz");
+    ArrayList<String> exp1 = new ArrayList<String>();
+    exp1.add("dcba");
+    exp1.add("zyx");
+    testReverseAll(list1, exp1);
+
+    ArrayList<String> list2 = new ArrayList<String>();
+    list2.add("1234");
+    list2.add("5678");
+    ArrayList<String> exp2 = new ArrayList<String>();
+    exp2.add("4321");
+    exp2.add("8765");
+    testReverseAll(list2, exp2);
+
+    ArrayList<String> list3 = new ArrayList<String>();
+    list3.add("racecar");
+    list3.add("tacocat");
+    list3.add("izzi");
+    ArrayList<String> exp3 = new ArrayList<String>();
+    exp3.add("racecar");
+    exp3.add("tacocat");
+    exp3.add("izzi");
+    testReverseAll(list3, exp3);
+  }
+}
